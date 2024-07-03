fbaca12eb8d1baf3928b295249ad168a2879d53f
sourabhaxworkz <sourabhayn@gmail.com>
2024-07-03 10:43:12 +0530

    sourabha

---
+++ Shoes.java
@@ -0,0 +1,16 @@
+class Shoes{
+	public static void main(String[] shoes){
+		String brandName = "puma";
+		String color = "Black";
+		int size = 6;
+		int price = 2000;
+		int brandId = 2569785;
+		String type = "sports";
+			System.out.println("The Brand Name is " + brandName);
+			System.out.println("The Size is " + size);
+			System.out.println("The Price is " + price);
+			System.out.println("The BrandId is " + brandId);
+			System.out.println("The Type of shoes is " + type);
+			System.out.println("The Color of shoes is " + color);
+	}
+}
