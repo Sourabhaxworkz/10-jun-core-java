c605f0969e14990fea285af872eab409cbd8eff9
sourabhaxworkz <sourabhayn@gmail.com>
2024-07-04 10:47:03 +0530

    Basics Java Program

--- Shoes.java
+++ Shoes.java
@@ -1,16 +1,7 @@
 class Shoes{
-	public static void main(String[] shoes){
-		String brandName = "puma";
-		String color = "Black";
-		int size = 6;
-		int price = 2000;
-		int brandId = 2569785;
-		String type = "sports";
-			System.out.println("The Brand Name is " + brandName);
-			System.out.println("The Size is " + size);
-			System.out.println("The Price is " + price);
-			System.out.println("The BrandId is " + brandId);
-			System.out.println("The Type of shoes is " + type);
-			System.out.println("The Color of shoes is " + color);
+	public static void main(String[] asianmenshoes){
+		System.out.println("Lightweight & Breathable : Exclusive design and durable materials every step feels light and breezy. Breathable, free-moving fabrics which adjust according to your foot and creates an astoundingly easy-going experience.");
+		System.out.println("Non Slip & Shockproof : Great engineering strikes a balance in style, made in the potent design and latest fashion trends. Made for long-term wear, with extra emphasis on providing cushion to the feet, removing heel strain.");
+		System.out.println("Comfort Sole & Flexible Walk : The outsoles are made by an air cushion, doubling the effect of shock absorption. Besides, these shoes perform excellent in durability and are also slip resistant. It provides push cushioning comfort for foot pain relief and helps relieve pressure while conforming to your every");
 	}
 }
---
+++ Trees.java
@@ -0,0 +1,9 @@
+class Trees{
+	public static void main(String[] mango){
+		System.out.println("A mango is an edible stone fruit produced by the tropical tree Mangifera indica");
+		System.out.println("It originated from the region between northwestern Myanmar, Bangladesh, and northeastern India");
+		System.out.println("indica has been cultivated in South and Southeast Asia since ancient times resulting in two types of modern mango cultivars");
+		System.out.println("Other species in the genus Mangifera also produce edible fruits that are also called mangoes, the majority of which are found in the Malesian ecoregion");
+	
+	}
+}
