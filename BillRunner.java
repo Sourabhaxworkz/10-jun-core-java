bf2b62d8623bebd2e85dd763fecc3cc31756f296
Sourabhaxworkz <sourabhayn@gmail.com>
2024-07-30 19:59:16 +0530

    Class And Object Program

---
+++ BillRunner.java
@@ -0,0 +1,302 @@
+class BillRunner{
+	public static void main(String args[]){
+		//className ref = new className();
+		
+		Bill ref = new Bill();
+		System.out.println("Bill Sequence number is started");
+		ref.billSequenceNo = 1;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo = 2;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo = 3;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo = 4;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo=5;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo=6;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo=7;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo=8;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo=9;
+		System.out.println(ref.billSequenceNo);
+		
+		ref.billSequenceNo=10;
+		System.out.println(ref.billSequenceNo);
+		System.out.println("Bill Sequence number is ended");
+		
+		Bill ref1 = new Bill();
+		System.out.println("Bill Reference ID is Started");
+		ref1.billReferenceId = 1236;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=1243;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=1235;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=1258;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=1478;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=9874;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=8521;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=8524;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=7896;
+		System.out.println(ref1.billReferenceId);
+		
+		ref1.billReferenceId=4523;
+		System.out.println(ref1.billReferenceId);
+		System.out.println("Bill Reference ID is Ended");
+		
+		Bill ref2 = new Bill();
+		System.out.println("Bill Issue Date is Started");
+		ref2.billIssueDate = "12/5/2024";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/4/2024";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/3/2024";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/2/2024";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/1/2024";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/12/2023";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/11/2023";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/10/2023";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/09/2023";
+		System.out.println(ref2.billIssueDate);
+		
+		ref2.billIssueDate = "12/08/2023";
+		System.out.println(ref2.billIssueDate);
+		System.out.println("Bill Issue Date is Ended");
+		
+		Bill ref3 = new Bill();
+		System.out.println("Bill Due Date is Started");
+		ref3.billDueDate = "22/5/2024";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/4/2024";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/3/2024";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/2/2024";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/1/2024";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/12/2023";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/11/2023";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/10/2023";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/09/2023";
+		System.out.println(ref3.billDueDate);
+		
+		ref3.billDueDate = "22/08/2023";
+		System.out.println(ref3.billDueDate);
+		System.out.println("Bill Due Date is Ended");
+		
+		Bill ref4 = new Bill();
+		System.out.println("Bill Paid is started");
+		ref4.isBillPaid = false;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		
+		ref4.isBillPaid = true;
+		System.out.println(ref4.isBillPaid);
+		System.out.println("Bill Paid is Ended");
+		
+		Bill ref5 = new Bill();
+		System.out.println("Latest Bill is started");
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		
+		ref5.isLatestBill = true;
+		System.out.println(ref5.isLatestBill);
+		System.out.println("Latest Bill is Ended");
+		
+		Bill ref6 = new Bill();
+		System.out.println("Final Bill Started");
+		ref6.isFinalBill = true;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		
+		ref6.isFinalBill = false;
+		System.out.println(ref6.isFinalBill);
+		System.out.println("Final Bill Ended");
+		
+		Bill ref7 = new Bill();
+		System.out.println("Bill Amount is Started");
+		ref7.billAmount = 256.3;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 456.6;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 789.5;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 1236.5;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 963.2;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 4563.2;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 789.3;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 456.2;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 741.2;
+		System.out.println(ref7.billAmount);
+		
+		ref7.billAmount = 852.3;
+		System.out.println(ref7.billAmount);
+		System.out.println("Bill Amount is Ended");
+		
+		Bill ref8 = new Bill();
+		System.out.println("Tax Inclusive is Started");
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		
+		ref8.tacInclusiveBill = true;
+		System.out.println(ref8.tacInclusiveBill);
+		System.out.println("Tax Inclusive is Ended");
+	}
+}
